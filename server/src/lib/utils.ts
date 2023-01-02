6e62a3f7f1fa965c3ec632fe07b14d789ec78a63
renzynx <69468436+renzynx@users.noreply.github.com>
2023-01-02 15:52:43 +0000

    final fix

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,5 +1,4 @@
 export const serialize = (s: string) => {
   const url = s.replace(/(^\w+:|^)\/\//, "");
-  const domain = url.split(".").slice(-2).join(".");
-  return domain.replace(/\/$/, "");
+  return url.split(".").slice(-2).join(".");
 };
